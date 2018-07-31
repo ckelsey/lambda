8ba743b0caab7506dc592c5ee7a796ef6a2e1af3
chris kelsey <chris@ckcreates.com>
2018-07-31 01:32:27 -0700

    images

--- swg/images/zipS3/app.js
+++ swg/images/zipS3/app.js
@@ -11,19 +11,18 @@ var XmlStream = require('xml-stream')
 
 var region = 'us-east-1'
 var bucket = 'cklsymedia'
-var folder = 'super_res'
+// var folder = 'super_res'
 AWS.config.loadFromPath('./config.json');
 var s3 = new AWS.S3({ region: region })
 var params = {
     Bucket: bucket,
-    Prefix: folder
+    // Prefix: folder
 }
 // var page = process.env.PAGE
 // var count = process.env.COUNT
 var zipName = `super_res.zip`
 
 const http = require('http')
-const func = require("./index")
 const server = http.createServer().listen(8127);
 
 server.on("request", (req, res) => {
@@ -34,25 +33,33 @@ server.on("request", (req, res) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
+    if(req.url !== `/`){
+        res.statusCode = 200
+        res.end()
+        return
+    }
+
     console.log(`handle`)
     var filesArray = []
     var files = s3.listObjects(params).createReadStream()
     var xml = new XmlStream(files)
     xml.collect('Key')
     xml.on('endElement: Key', function (item) {
-        filesArray.push(item['$text'].substr(folder.length))
+        // filesArray.push(item['$text'].substr(folder.length))
+        filesArray.push(item['$text'].substr('super_res'.length))
     })
 
     xml
         .on('end', function () {
-            zip(filesArray)
+            zip(filesArray.slice(0,100))
         })
 
     function zip(files) {
+        console.log(filesArray.length)
         var output = fs.createWriteStream(zipName)
 
         s3Zip
-            .archive({ region: region, bucket: bucket, preserveFolderStructure: true }, folder, files)
+            .archive({ region: region, bucket: bucket, preserveFolderStructure: true }, `/`, files)
             .pipe(output)
             .on(`finish`, () => {
                 s3.putObject({
