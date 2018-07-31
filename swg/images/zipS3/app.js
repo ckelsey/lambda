3cccf2ee7546e900eaf679268d0760a6bd0c6237
chris kelsey <chris@ckcreates.com>
2018-07-31 00:55:09 -0700

    images

--- swg/images/zipS3/app.js
+++ swg/images/zipS3/app.js
@@ -33,56 +33,43 @@ server.on("request", (req, res) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-    req.on('end', () => {
-        // let data = body
+    console.log(`handle`)
+    var filesArray = []
+    var files = s3.listObjects(params).createReadStream()
+    var xml = new XmlStream(files)
+    xml.collect('Key')
+    xml.on('endElement: Key', function (item) {
+        filesArray.push(item['$text'].substr(folder.length))
+    })
 
-        // try { data = JSON.parse(data) } catch (error) { }
-
-        // func.handler(null, {}, (error, result) => {
-            // res.statusCode = 200
-            // res.write(JSON.stringify({ error, result }))
-            // res.end()
-            // return
-        // })
-
-        console.log(`handle`)
-        var filesArray = []
-        var files = s3.listObjects(params).createReadStream()
-        var xml = new XmlStream(files)
-        xml.collect('Key')
-        xml.on('endElement: Key', function (item) {
-            filesArray.push(item['$text'].substr(folder.length))
+    xml
+        .on('end', function () {
+            zip(filesArray)
         })
 
-        xml
-            .on('end', function () {
-                zip(filesArray)
-            })
+    function zip(files) {
+        console.log(files)
+        var output = fs.createWriteStream(join('/tmp', zipName))
 
-        function zip(files) {
-            console.log(files)
-            var output = fs.createWriteStream(join('/tmp', zipName))
-
-            s3Zip
-                .archive({ region: region, bucket: bucket, preserveFolderStructure: true }, folder, files)
-                .pipe(output)
-                .on(`finish`, () => {
-                    s3.putObject({
-                        "Body": fs.createReadStream(join('/tmp', zipName)),
-                        "Bucket": bucket,
-                        "Key": zipName,
-                        "Metadata": {
-                            "Content-Length": String(fs.statSync(join('/tmp', zipName)).size)
-                        }
-                    })
-                        .promise()
-                        .then(data => {
-                            res.statusCode = 200
-                            res.write(JSON.stringify(data))
-                            res.end()
-                            return
-                        })
+        s3Zip
+            .archive({ region: region, bucket: bucket, preserveFolderStructure: true }, folder, files)
+            .pipe(output)
+            .on(`finish`, () => {
+                s3.putObject({
+                    "Body": fs.createReadStream(join('/tmp', zipName)),
+                    "Bucket": bucket,
+                    "Key": zipName,
+                    "Metadata": {
+                        "Content-Length": String(fs.statSync(join('/tmp', zipName)).size)
+                    }
                 })
-        }
-    })
+                    .promise()
+                    .then(data => {
+                        res.statusCode = 200
+                        res.write(JSON.stringify(data))
+                        res.end()
+                        return
+                    })
+            })
+    }
 })
